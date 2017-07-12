de2198d520088c325f043f4e8191d4d5c1e2934a
zlindner <zach.lindner@hotmail.com>
2017-07-12 18:48:22 -0400

    Base mod files

---
+++ src/main/java/zachy/ultio/Ultio.java
@@ -0,0 +1,25 @@
+package zachy.ultio;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "ultio", name = "Ultio", version = "0.0.1")
+public class Ultio {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
